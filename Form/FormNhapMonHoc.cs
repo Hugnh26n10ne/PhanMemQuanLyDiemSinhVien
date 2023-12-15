ce35c61dacb5bca207fac3273b4ca7b35c383fd4
Ngluong0813 <ngluong0813@gmail.com>
2023-12-15 08:29:27 +0700

    update Nhập môn học #2

--- Form/FormNhapMonHoc.cs
+++ Form/FormNhapMonHoc.cs
@@ -21,5 +21,10 @@ namespace PhanMemQuanLyDiemSinhVien
         {
 
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
