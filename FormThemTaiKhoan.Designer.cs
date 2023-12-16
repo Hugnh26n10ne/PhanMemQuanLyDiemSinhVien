d1e8f19cc3144df8f09061759198c9d64f216acb
ASUS <ASUS@LAPTOP-35VC7TDS>
2023-12-16 23:09:19 +0700

    Create Form Them Tai Khoan

---
+++ FormThemTaiKhoan.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace PhanMemQuanLyDiemSinhVien
+{
+    partial class FormThemTaiKhoan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dtgrv_BangThongTIn = new System.Windows.Forms.DataGridView();
+            this.TaiKhoan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MatKhau = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ChucVu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TrangThai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_Them = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrv_BangThongTIn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btn_Them);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(102, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1389, 298);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(180, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tài Khoản";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(180, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật Khẩu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(340, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(465, 31);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(340, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(465, 31);
+            this.textBox2.TabIndex = 7;
+            // 
+            // dtgrv_BangThongTIn
+            // 
+            this.dtgrv_BangThongTIn.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dtgrv_BangThongTIn.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgrv_BangThongTIn.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TaiKhoan,
+            this.MatKhau,
+            this.ChucVu,
+            this.TrangThai});
+            this.dtgrv_BangThongTIn.Location = new System.Drawing.Point(102, 467);
+            this.dtgrv_BangThongTIn.Name = "dtgrv_BangThongTIn";
+            this.dtgrv_BangThongTIn.RowHeadersWidth = 82;
+            this.dtgrv_BangThongTIn.RowTemplate.Height = 33;
+            this.dtgrv_BangThongTIn.Size = new System.Drawing.Size(1389, 499);
+            this.dtgrv_BangThongTIn.TabIndex = 1;
+            // 
+            // TaiKhoan
+            // 
+            this.TaiKhoan.HeaderText = "Tài Khoản ";
+            this.TaiKhoan.MinimumWidth = 10;
+            this.TaiKhoan.Name = "TaiKhoan";
+            this.TaiKhoan.Width = 300;
+            // 
+            // MatKhau
+            // 
+            this.MatKhau.HeaderText = "Mật Khẩu";
+            this.MatKhau.MinimumWidth = 10;
+            this.MatKhau.Name = "MatKhau";
+            this.MatKhau.Width = 300;
+            // 
+            // ChucVu
+            // 
+            this.ChucVu.HeaderText = "Chức Vụ";
+            this.ChucVu.MinimumWidth = 10;
+            this.ChucVu.Name = "ChucVu";
+            this.ChucVu.Width = 300;
+            // 
+            // TrangThai
+            // 
+            this.TrangThai.HeaderText = "Trạng Thái";
+            this.TrangThai.MinimumWidth = 10;
+            this.TrangThai.Name = "TrangThai";
+            this.TrangThai.Width = 300;
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Them.Location = new System.Drawing.Point(184, 205);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(125, 44);
+            this.btn_Them.TabIndex = 8;
+            this.btn_Them.Text = "Thêm ";
+            this.btn_Them.UseVisualStyleBackColor = true;
+            this.btn_Them.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(384, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 44);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(584, 205);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 44);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(784, 205);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 44);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Xuất File";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(984, 205);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(128, 44);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Thoát";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // FormThemTaiKhoan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1672, 1085);
+            this.Controls.Add(this.dtgrv_BangThongTIn);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormThemTaiKhoan";
+            this.Text = "FormThemTaiKhoan";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrv_BangThongTIn)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dtgrv_BangThongTIn;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_Them;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TaiKhoan;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MatKhau;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ChucVu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TrangThai;
+    }
+}
